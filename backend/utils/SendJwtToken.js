d3b178d6d8e2c1d330a21c12e86474184704ac0c
ishanlathwal <ishan.ascent@gmail.com>
2025-07-04 15:05:49 +0530

    cookies

--- backend/utils/SendJwtToken.js
+++ backend/utils/SendJwtToken.js
@@ -1,13 +1,13 @@
 const sendJwtToken = (user, statusCode, res) => {
   const token = user.generateJWT();
-
+  const isProd = process.env.NODE_ENV === "production";
   const options = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-
+    secure: isProd,
     httpOnly: true,
-    sameSite: "None",
+    sameSite: isProd ? "None" : "Lax",
   };
 
   user.password = undefined;
