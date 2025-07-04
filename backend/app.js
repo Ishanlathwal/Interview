559e6d111c4f24c3e6384b057ed8db3db9273778
ishanlathwal <ishan.ascent@gmail.com>
2025-07-04 13:52:13 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.set("query parser", "extended");
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-fjkj.onrender.com",
     credentials: true,
   })
 );
