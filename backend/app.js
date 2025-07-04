7d8043f5b940faa6e035c5f40a27cc712ebfd1f8
ishanlathwal <ishan.ascent@gmail.com>
2025-07-04 15:38:09 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,9 @@ process.on("uncaughtException", (err) => {
 
 const app = express();
 app.set("query parser", "extended");
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+}
 
 // app.use(cors());
 const allowedOrigins = [
